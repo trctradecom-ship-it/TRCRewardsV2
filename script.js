8f6b70a33da08cd3d31da995a7beb81e034a0165
Tradecom <trctradecom@gmail.com>
2026-03-27 21:07:54 +0530

    Update script.js

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ async function connectWallet() {
     token = new ethers.Contract(tokenAddress, tokenABI, signer);
 
     loadData();
-    setInterval(loadData, 10000);
+    setInterval(loadData, 60000); 
 
     startTimers(); // ✅ ADDED
     listenEvents();
